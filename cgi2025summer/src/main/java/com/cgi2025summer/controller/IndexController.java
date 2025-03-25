ebe58953a6adb6cda924c1bc4de8aff8b5168735
asdfguy <karlike231@gmail.com>
2025-03-25 20:03:29 +0000

    changed templates from JSP-s to Thymeleaf templates

--- cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.cgi2025summer.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cgi2025summer.model.Flight;
+import com.cgi2025summer.model.FlightDataGenerator;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.jsp";
+    public String index(Model model) {
+        ArrayList<Flight> flights = new FlightDataGenerator().getFlightsData();
+        flights.sort((arg0, arg1) -> arg0.getDepartureTimeString().compareTo(arg1.getDepartureTimeString()));
+        model.addAttribute("flights", flights);
+        return "index";
     }
 }
