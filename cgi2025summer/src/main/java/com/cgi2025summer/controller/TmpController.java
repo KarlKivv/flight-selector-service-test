0bff1afd2a04ee7184bca8ab3fb1371a144ee8ec
asdfguy <karlike231@gmail.com>
2025-03-26 22:59:23 +0000

    fixed missing tmp controller annotation, added ISO code search

--- cgi2025summer/src/main/java/com/cgi2025summer/controller/TmpController.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/controller/TmpController.java
@@ -1,7 +1,9 @@
 package com.cgi2025summer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class TmpController {
     @RequestMapping("/tmp")
     public String tmp() {
