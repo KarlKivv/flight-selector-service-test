e81fe34a95d19e5e035e8b4000b855eeccda0cb9
asdfguy <karlike231@gmail.com>
2025-03-28 23:22:23 +0000

    changed from deparate departureHour and departureMinute to Calendar date

--- cgi2025summer/src/main/java/com/cgi2025summer/model/Flight.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/model/Flight.java
@@ -1,27 +1,38 @@
 package com.cgi2025summer.model;
 
+import java.util.Calendar;
 import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
 @Component
-public class Flight {
+public class Flight implements Comparable<Flight> {
     private String id;
     private String destination;
 
-    private long departureHour;
-    private long departureMinute;
+    private Calendar departureDate;
+
+    // private long departureHour;
+    // private long departureMinute;
 
     public Flight() {
         this.id = UUID.randomUUID().toString();
         Random random = new Random();
         this.destination = FlightDestinationsEnum.values()[Math
-                .toIntExact(random.nextLong(FlightDestinationsEnum.values().length))]
-                .toString();
+                .toIntExact(random.nextLong(FlightDestinationsEnum.values().length))].toString();
+        long departureHour = random.nextLong(24);
+        long departureMinute = random.nextLong(60);
 
-        this.departureHour = random.nextLong(24);
-        this.departureMinute = random.nextLong(60);
+        // SimpleDateFormat formatter = new SimpleDateFormat("dd MMM YYYY HH:mm",
+        // Locale.ENGLISH);
+        int departureHourAsInt = Math.toIntExact(departureHour);
+        int departureMinuteAsInt = Math.toIntExact(departureMinute);
+
+        Calendar instance = Calendar.getInstance();
+        instance.set(Calendar.HOUR_OF_DAY, departureHourAsInt);
+        instance.set(Calendar.MINUTE, departureMinuteAsInt);
+        this.departureDate = instance;
     }
 
     public String getId() {
@@ -34,17 +45,60 @@ public class Flight {
 
     // tried to use Date and Calendar types, but they seem to be depracated since
     // Java 8+ and I got tired of messing with Temporal-s
+    // public String getDepartureTimeString() {
+    // StringBuilder builder = new StringBuilder();
+    // if (this.departureHour < 10) {
+    // builder.append(0);
+    // }
+    // builder.append(this.departureHour);
+    // builder.append(":");
+    // if (this.departureMinute < 10) {
+    // builder.append(0);
+    // }
+    // builder.append(this.departureMinute);
+    // return builder.toString();
+    // }
+
     public String getDepartureTimeString() {
-        StringBuilder builder = new StringBuilder();
-        if (this.departureHour < 10) {
-            builder.append(0);
-        }
-        builder.append(this.departureHour);
-        builder.append(":");
-        if (this.departureMinute < 10) {
-            builder.append(0);
-        }
-        builder.append(this.departureMinute);
-        return builder.toString();
+        return String.format("%1$tH:%1$tM", this.departureDate);
     }
+
+    @Override
+    public int compareTo(Flight arg0) {
+        if (this.id.equals(arg0.id)) {
+            return 0;
+        }
+        int destComp = this.destination.compareTo(arg0.destination);
+        if (destComp != 0) {
+            return destComp;
+        }
+
+        int calendarComp = this.departureDate.compareTo(arg0.departureDate);
+        if (calendarComp != 0) {
+            if (calendarComp > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        // long hourComp = this.departureHour - arg0.departureHour;
+        // if (hourComp != 0) {
+        // if (hourComp > 0) {
+        // return 1;
+        // } else {
+        // return -1;
+        // }
+        // }
+        // long minuteComp = this.departureMinute - arg0.departureMinute;
+        // if (minuteComp != 0) {
+        // if (minuteComp > 0) {
+        // return 1;
+        // } else {
+        // return -1;
+        // }
+        // }
+        return this.id.compareTo(arg0.id);
+    }
+
 }
