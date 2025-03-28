e40edf565eae495b06ad6e7ffbcc410c71044b04
asdfguy <karlike231@gmail.com>
2025-03-28 23:23:16 +0000

    changed indexController to use flightService in stead of directly generating the data in the endpoint controller

--- cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
@@ -1,22 +1,41 @@
 package com.cgi2025summer.controller;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cgi2025summer.model.Flight;
-import com.cgi2025summer.model.FlightDataGenerator;
+import com.cgi2025summer.model.FlightDestinationsEnum;
+import com.cgi2025summer.service.FlightsService;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private FlightsService service;
+
     @RequestMapping("/")
-    public String index(Model model) {
-        ArrayList<Flight> flights = new FlightDataGenerator().getFlightsData();
-        flights.sort((arg0, arg1) -> arg0.getDepartureTimeString().compareTo(arg1.getDepartureTimeString()));
+    public String index(
+            Model model,
+            @RequestParam(name = "date", required = false) @DateTimeFormat(pattern = "MM/dd/yyyy") Calendar date,
+            @RequestParam(name = "destination", required = false, defaultValue = "") FlightDestinationsEnum destination) {
+
+        ArrayList<Flight> flights = this.service.getFlightsByDate(Calendar.getInstance());
         model.addAttribute("flights", flights);
         return "index";
     }
+
+    @GetMapping("/flights/{flightId}")
+    public String getMethodName(@RequestParam String flightId) {
+        return new String();
+    }
+
 }
