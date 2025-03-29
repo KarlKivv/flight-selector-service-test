b6443c06d0799e070952d5388b22c5fdd7f57128
asdfguy <karlike231@gmail.com>
2025-03-29 23:48:08 +0000

    renamed AirplaneSeat to Seat

---
+++ cgi2025summer/src/main/java/com/cgi2025summer/model/Seat.java
@@ -0,0 +1,52 @@
+package com.cgi2025summer.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class Seat {
+
+    @Autowired
+    private SeatColumnEnum column;
+
+    private int row;
+    private boolean available;
+    private int attractionScore;
+
+    public Seat(int row, SeatColumnEnum column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public SeatColumnEnum getColumn() {
+        return column;
+    }
+
+    public void reserveSeat() {
+        this.available = false;
+    }
+
+    public void unreserveSeat() {
+        this.available = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %d", this.column.toString(), this.row);
+    }
+
+    public void setColumn(SeatColumnEnum column) {
+        this.column = column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+}
