772b0ab7b420fb996360632a899b8c53995c9e79
asdfguy <karlike231@gmail.com>
2025-03-29 14:31:59 +0000

    changed storage hashmap key from Calendar to formatted date String

--- cgi2025summer/src/main/java/com/cgi2025summer/model/DataStorage.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/model/DataStorage.java
@@ -1,5 +1,6 @@
 package com.cgi2025summer.model;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -8,21 +9,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataStorage {
-    private HashMap<Calendar, ArrayList<Flight>> flightsByDate;
+    private HashMap<String, ArrayList<Flight>> flightsByDate;
+    private SimpleDateFormat format;
 
     public DataStorage() {
         this.flightsByDate = new HashMap<>();
+        this.format = new SimpleDateFormat("dd/MM/yyyy");
     }
 
     public void add(Calendar date, ArrayList<Flight> flights) {
-        this.flightsByDate.put(date, flights);
+        this.flightsByDate.put(formatDate(date), flights);
     }
 
     public ArrayList<Flight> getFlightsByDate(Calendar date) {
-        return this.flightsByDate.get(date);
+        return this.flightsByDate.get(formatDate(date));
     }
 
-    public HashMap<Calendar, ArrayList<Flight>> getAllFlights() {
+    public HashMap<String, ArrayList<Flight>> getAllFlights() {
         return this.flightsByDate;
+    }
+
+    public SimpleDateFormat getDateFormatter() {
+        return this.format;
+    }
+
+    private String formatDate(Calendar date) {
+        return format.format(date.getTime());
     }
 }
