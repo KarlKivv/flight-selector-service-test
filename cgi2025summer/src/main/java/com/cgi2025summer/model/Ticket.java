6c9177193252611ef7e75b1924c91e5915c9120b
asdfguy <karlike231@gmail.com>
2025-03-30 00:34:13 +0000

    removed unused class

--- cgi2025summer/src/main/java/com/cgi2025summer/model/Ticket.java
+++
@@ -1,8 +0,0 @@
-package com.cgi2025summer.model;
-
-public class Ticket {
-    private double cost;
-    private Flight flight;
-    private int attractionScore; // this is what tracks the similarity between the user's filters
-    private AirplaneSeat seat;
-}
