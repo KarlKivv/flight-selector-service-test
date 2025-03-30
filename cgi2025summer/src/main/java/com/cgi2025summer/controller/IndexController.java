2f41cc54a6ad8e2eeef3d13df25e884e47cabe63
asdfguy <karlike231@gmail.com>
2025-03-30 20:51:20 +0000

    separated dateFilter and locationFilter parameters

--- cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
+++ cgi2025summer/src/main/java/com/cgi2025summer/controller/IndexController.java
@@ -1,21 +1,16 @@
 package com.cgi2025summer.controller;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cgi2025summer.model.Flight;
-import com.cgi2025summer.model.FlightDestinationsEnum;
 import com.cgi2025summer.service.FlightsService;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,20 +21,23 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        ArrayList<Flight> flights = service.filterFlights("");
+        ArrayList<Flight> flights = service.filterFlights("", "");
         model.addAttribute("flights", flights);
         return "index";
     }
 
     @PostMapping("/")
-    public String filter(@RequestParam String filter, Model model) {
-        ArrayList<Flight> flights = service.filterFlights(filter);
+    public String filterFlights(
+            @RequestParam(required = false, defaultValue = "") String dateFilter,
+            @RequestParam(required = false, defaultValue = "") String destinationFilter,
+            Model model) {
+        ArrayList<Flight> flights = service.filterFlights(dateFilter, destinationFilter);
         model.addAttribute("flights", flights);
         return "index";
     }
 
     @GetMapping("/flight")
-    public String getMethodName(@RequestParam String id, Model model) {
+    public String flightPage(@RequestParam(defaultValue = "") String id, Model model) {
         Flight flight = service.getFlightById(id);
         if (flight == null) {
             model.addAttribute("id", id);
