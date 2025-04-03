8fb47a4e06ba24cf12f59b60e00702cacf0df76d
asdfguy <karlike231@gmail.com>
2025-04-03 22:27:17 +0000

    changed dateTimeFilterStart and dateTimeFilterEnd to Calendar types, added flights arraylist as a field

--- flightSelectorDemo/src/main/java/com/flightSelectorDemo/dto/FlightFilterDTO.java
+++ flightSelectorDemo/src/main/java/com/flightSelectorDemo/dto/FlightFilterDTO.java
@@ -1,8 +1,16 @@
 package com.flightSelectorDemo.dto;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.flightSelectorDemo.model.Flight;
+
 public record FlightFilterDTO(
-        String destinationFilter,
-        String dateTimeFilterStart,
-        String dateTimeFilterEnd,
-        String dateFilter) {
+                String destinationFilter,
+                @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") Calendar dateTimeFilterStart,
+                @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") Calendar dateTimeFilterEnd,
+                String dateFilter,
+                ArrayList<Flight> flights) {
 }
